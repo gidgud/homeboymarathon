451abcd3f86ef70e1cc8c4769c88088119c46ba2
gidgud <jonathanbirkstrom@gmail.com>
2026-05-01 10:48:58 +0200

    Initial commit

---
+++ src/main/java/dev/guts/hbmarathon/HbmarathonApplication.java
@@ -0,0 +1,13 @@
+package dev.guts.hbmarathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbmarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbmarathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/guts/hbmarathon/HbmarathonApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guts.hbmarathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbmarathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
