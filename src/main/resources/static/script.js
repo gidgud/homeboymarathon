e06a3d87a8e2d86964eb904aa5a7bb773482fd1e
Yakoob1884 <vivja.aty@gmail.com>
2026-05-06 12:15:20 +0200

    JS login + create user

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -3,4 +3,9 @@ function showPage(name) {
 	document.querySelectorAll('.page').forEach(a => a.classList.add('hidden'));
 	document.getElementById(name).classList.remove('hidden');
 
+	if(name === 'login-page') {
+		initLoginPage();
+	} else if (name === 'create-page') {
+		initCreatePage();
+	}
 }
