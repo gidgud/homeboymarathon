5f6e77b6acae040df4cad2c162484b65d6101a01
Yakoob1884 <vivja.aty@gmail.com>
2026-05-07 14:13:51 +0200

    Created a functual create user function

--- src/main/resources/static/pages/create.js
+++ src/main/resources/static/pages/create.js
@@ -10,22 +10,22 @@ async function initCreatePage() {
     title.innerHTML = "Opret bruger"
 
     const firstName = document.createElement("input");
-    firstName.type = "firstName"
+    firstName.type = "text"
     firstName.placeholder = "Fornavn"
     firstName.id = "firstName"
 
     const lastName = document.createElement("input");
-    lastName.type = "lastName";
+    lastName.type = "text";
     lastName.placeholder = "Efternavn";
     lastName.id = "lastName";
 
     const birthday = document.createElement("input");
     birthday.type = "date";
     birthday.placeholder = "YYYY-MM-DD";
-    birthday.id = "birthday";
+    birthday.id = "dateOfBirth";
 
     const phoneNumber = document.createElement("input");
-    phoneNumber.type = "phoneNumber";
+    phoneNumber.type = "tel";
     phoneNumber.placeholder = "4512345678";
     phoneNumber.id = "phoneNumber";
 
@@ -41,9 +41,45 @@ async function initCreatePage() {
 
     const createButton = document.createElement("button");
     createButton.innerText = "Opret";
-    createButton.onclick = "create";
+    createButton.addEventListener("click", createUser);
 
     container.append(title, firstName, lastName, birthday, phoneNumber, email, password, createButton);
     page.appendChild(container);
 
+}
+
+async function createUser (){
+    const user = {
+        firstName: document.getElementById("firstName").value,
+        lastName: document.getElementById("lastName").value,
+        dateOfBirth: document.getElementById("dateOfBirth").value,
+        phoneNumber: document.getElementById("phoneNumber").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+
+    try {
+
+        const response = await fetch ("http://localhost:8080/api/users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(user)
+        });
+        if (response.ok) {
+            alert("Bruger oprettet! :D");
+            showPage("login-page")
+        } else {
+            const errorText = await response.text();
+
+            console.log(response.status);
+            console.log(errorText);
+
+            alert("Kunne ikke oprette bruger")
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Server fejl ;_;")
+    }
 }
